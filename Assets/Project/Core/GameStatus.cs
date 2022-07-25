3c990b66e8c4a9e6b78f2e32f4f90e48cf60fd55
Denys Zhytnyk <56601759+DOG007ua@users.noreply.github.com>
2022-07-25 14:35:06 +0300

    Create gameStatus

---
+++ Assets/Project/Core/GameStatus.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus
+{
+    public int Level { get; private set; }
+    public int TotalSecondsGame => (int)(DateTime.Now - timeStartGame).TotalSeconds;
+    private DateTime timeStartGame;
+
+    public GameStatus()
+    {
+        timeStartGame = DateTime.Now;
+        Level = 1;
+    }
+}
