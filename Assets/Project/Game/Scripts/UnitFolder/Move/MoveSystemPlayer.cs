598fe6b77e13e521b02b38fa28822ea9c3fc4ebc
Denys Zhytnyk <56601759+DOG007ua@users.noreply.github.com>
2022-07-25 15:17:52 +0300

    Create Move system Player

--- Assets/Project/Game/Scripts/UnitFolder/Move/MoveSystemPlayer.cs
+++ Assets/Project/Game/Scripts/UnitFolder/Move/MoveSystemPlayer.cs
@@ -2,28 +2,10 @@
 
 namespace Project.Game.Scripts.UnitFolder.Move
 {
-    public class MoveSystemPlayer : IMoveSystem
+    public class MoveSystemPlayer : MoveSystem
     {
-        public void Execute()
+        public MoveSystemPlayer(Transform transform, Unit unit) : base(transform, unit)
         {
-            throw new System.NotImplementedException();
         }
-
-        public void MoveToPosition(Vector3 position)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void MoveToDirection(Vector3 vector3)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Stop()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public bool IsBlockMove { get; set; }
     }
 }
